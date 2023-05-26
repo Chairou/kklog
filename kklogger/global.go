7e80f4f9e97b5c3828e16c3471d4efec2b2ef599
chairou <chairou@tencent.com>
2023-05-27 01:04:31 +0800

    Initial commit2

--- kklogger/global.go
+++ kklogger/global.go
@@ -1,3 +1,20 @@
 package kklogger
 
-const ()
+import "sync"
+
+// severity identifies the sort of log: info, warning etc. The binding to flag.Value
+// is handled in klog.go
+type Severity int32 // sync/atomic int32
+
+// These constants identify the log levels in order of increasing severity.
+// A message written to a high-severity log file is also written to each
+// lower-severity log file.
+const (
+	InfoLog Severity = iota
+	WarningLog
+	ErrorLog
+	FatalLog
+	NumSeverity = 4
+)
+
+var kkmap sync.Map
